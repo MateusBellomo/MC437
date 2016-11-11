af1fe3ce2355c8967d27d825afe0effe7b351768
Julio Barros <juliobarrosdepaula@gmail.com>
2016-11-11 21:56:03 -0200

    hibernate config

---
+++ SADE-Backend/src/unicamp/mc437/persistance/Desenvolvedor.java
@@ -0,0 +1,51 @@
+package unicamp.mc437.persistance;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Desenvolvedor")
+public class Desenvolvedor {
+
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	
+	private String name;
+	
+	private String info;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	
+	@Override
+	public String toString(){
+		return "id="+id+", name="+name+", info="+info;
+	}
+}
