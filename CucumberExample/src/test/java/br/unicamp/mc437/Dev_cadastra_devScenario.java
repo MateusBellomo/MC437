79890f6c069340e905bb672cab92b1d3eb26e197
autotelicbrown <autotelicbrown@gmail.com>
2016-11-11 22:45:00 -0200

    update dev cadastra dev scenario

--- CucumberExample/src/test/java/br/unicamp/mc437/Dev_cadastra_devScenario.java
+++ CucumberExample/src/test/java/br/unicamp/mc437/Dev_cadastra_devScenario.java
@@ -1,19 +1,23 @@
 package br.unicamp.mc437;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
 import java.io.File;
 import java.io.IOException;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import cucumber.annotation.After;
 import cucumber.annotation.Before;
 import cucumber.annotation.en.Given;
 import cucumber.annotation.en.Then;
 import cucumber.annotation.en.When;
+import cucumber.annotation.en_scouse.An;
 
 public class Dev_cadastra_devScenario {
     private WebDriver driver;
@@ -30,27 +34,50 @@ public class Dev_cadastra_devScenario {
     public void tearDown(){
     	driver.close();
     }
-
-    @Given("^the page is open \"([^\"]*)\"$")
-    public void the_page_is_open(String page) throws Throwable {
-    	driver.get(page);
-    }
-
-    @When("^I introduce my username \"([^\"]*)\" and password \"([^\"]*)\"$")
-    public void I_search_for(String username, String password) throws Throwable {
-    	WebElement username_f = driver.findElement(By.id("username"));
-    	username_f.sendKeys(username);
-    	WebElement password1 = driver.findElement(By.id("password1"));password1.sendKeys(password);
-    	WebElement password2 = driver.findElement(By.id("password2"));
-    	password2.sendKeys(password);
-    	password2.submit();
+    
+    @Given("^Estou na pagina de cadastrar dev$")
+    public void abrirPaginaCadastroDev() throws Throwable {
+    	//driver.get(page);
     }
     
-    @Then("^a page should show the message \"([^\"]*)\"$")
-    public void a_browser_title_should_contains(String text) throws Throwable {
-    	WebDriverWait wait = new WebDriverWait(driver, 15);
-    	wait.until(ExpectedConditions.presenceOfElementLocated(By.id("msg")));
-    	WebElement element = driver.findElement(By.id("msg"));
-    	assertThat(element.getText()).contains(text);
+    // And
+    @Given("^nao estou logado$")
+    public void checarNaoLogado() throws Throwable {
+    }
+
+    @Given("^Nenhum usuario esta cadastrado$")
+    public void nenhumCadastrado() throws Throwable {
+    }
+    
+    @When("^Eu preencho o campo usuario como \"([^\"]*)\"$")
+    public void preenchoCampoUsuarioCadastro(String username) throws Throwable {
+    }
+    
+    @When("^Preencho o restante dos campos com dados validos$")
+    public void preenchoCamposCadastro() throws Throwable {
+    }
+    
+    @Then("^Devo ir para a pagina confirmando cadastro de \"([^\"]*)\"$")
+    public void irPaginaConfirmacaoCadastro() throws Throwable {
+    }
+    
+    @When("^Preencho os campos de registro com dados invalidos$")
+    public void preencherCamposInvalidos() throws Throwable {
+    }
+    
+    @Then("^Recebo feedback de como preencher os campos corretamente$")
+    public void feedbackCamposRegistro() throws Throwable {
+    }
+    
+    @Given("^existem usuarios cadastrados$")
+    public void populateUsuarios() throws Throwable {
+    }
+    
+    @When("^tento cadastrar com dados conflitantes com usuarios cadastrados$")
+    public void cadastroConflitante() throws Throwable {
+    }
+    
+    @Then("^Sou informado dos dados conflitantes$")
+    public void informaConflito() throws Throwable {
     }
 }
