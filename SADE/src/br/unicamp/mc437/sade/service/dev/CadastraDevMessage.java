ab2aeae17ac79544a770550ecc95fff3b762e784
murilocruz <murilolcruz@gmail.com>
2016-12-02 20:17:02 -0200

    parse da mensagem de cadastro de dev

---
+++ SADE/src/br/unicamp/mc437/sade/service/dev/CadastraDevMessage.java
@@ -0,0 +1,145 @@
+package br.unicamp.mc437.sade.service.dev;
+
+public class CadastraDevMessage {
+	private String nome;
+	private String rg;
+	private String telRes;
+	private String telCel;
+	private String endereco;
+	private String instituicao;
+	private String curso;
+	private int hGrad;
+	private int hDisp;
+	private int hEstag;
+	private String curriculo;
+	private String observacoes;
+	private String conheceu;
+	private int java;
+	private int php;
+	private int mysql;
+	private int ajax;
+	private int javascript;
+	private int python;
+	private int cs;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getRg() {
+		return rg;
+	}
+	public void setRg(String rg) {
+		this.rg = rg;
+	}
+	public String getTelRes() {
+		return telRes;
+	}
+	public void setTelRes(String telRes) {
+		this.telRes = telRes;
+	}
+	public String getTelCel() {
+		return telCel;
+	}
+	public void setTelCel(String telCel) {
+		this.telCel = telCel;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getInstituicao() {
+		return instituicao;
+	}
+	public void setInstituicao(String instituicao) {
+		this.instituicao = instituicao;
+	}
+	public String getCurso() {
+		return curso;
+	}
+	public void setCurso(String curso) {
+		this.curso = curso;
+	}
+	public int gethGrad() {
+		return hGrad;
+	}
+	public void sethGrad(int hGrad) {
+		this.hGrad = hGrad;
+	}
+	public int gethDisp() {
+		return hDisp;
+	}
+	public void sethDisp(int hDisp) {
+		this.hDisp = hDisp;
+	}
+	public int gethEstag() {
+		return hEstag;
+	}
+	public void sethEstag(int hEstag) {
+		this.hEstag = hEstag;
+	}
+	public String getCurriculo() {
+		return curriculo;
+	}
+	public void setCurriculo(String curriculo) {
+		this.curriculo = curriculo;
+	}
+	public String getObservacoes() {
+		return observacoes;
+	}
+	public void setObservacoes(String observacoes) {
+		this.observacoes = observacoes;
+	}
+	public String getConheceu() {
+		return conheceu;
+	}
+	public void setConheceu(String conheceu) {
+		this.conheceu = conheceu;
+	}
+	public int getJava() {
+		return java;
+	}
+	public void setJava(int java) {
+		this.java = java;
+	}
+	public int getPhp() {
+		return php;
+	}
+	public void setPhp(int php) {
+		this.php = php;
+	}
+	public int getMysql() {
+		return mysql;
+	}
+	public void setMysql(int mysql) {
+		this.mysql = mysql;
+	}
+	public int getAjax() {
+		return ajax;
+	}
+	public void setAjax(int ajax) {
+		this.ajax = ajax;
+	}
+	public int getJavascript() {
+		return javascript;
+	}
+	public void setJavascript(int javascript) {
+		this.javascript = javascript;
+	}
+	public int getPython() {
+		return python;
+	}
+	public void setPython(int python) {
+		this.python = python;
+	}
+	public int getCs() {
+		return cs;
+	}
+	public void setCs(int cs) {
+		this.cs = cs;
+	}
+
+}
