c6273aaa30d4a1572b8b010258d6295e2f996f3a
Julio Barros de Paula <ra146752@led.lab.ic.unicamp.br>
2016-12-02 23:01:27 -0200

    banco h2

---
+++ SADE/src/br/unicamp/mc437/sade/DAO/GenericDAO.java
@@ -0,0 +1,14 @@
+package br.unicamp.mc437.sade.DAO;
+
+import java.util.List;
+import java.util.Map;
+
+public interface GenericDAO<T> {
+
+    public T get(Class<T> cl, Integer id);
+    public T save(T object);
+    public void update(T object);
+    public void delete(T object);
+    public List<T> query(String hsql, Map<String, Object> params);
+
+}
---
+++ SADE/src/br/unicamp/mc437/sade/persistance/Usuario.java
@@ -0,0 +1,51 @@
+package br.unicamp.mc437.sade.persistance;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="usuario")
+public class Usuario {
+	
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+
+	@Column(name="username")
+	private String name;
+
+	@Column(name="password")
+	private String password;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+}
